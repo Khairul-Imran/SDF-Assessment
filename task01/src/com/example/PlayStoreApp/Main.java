9f1dc38bc77be06aa268b7a7fe56a44dfdcbf83e
Khairul <Khairulimran6810@gmail.com>
2023-11-16 09:45:31 +0800

    Made additions to the PlaystoreData class

--- task01/src/com/example/PlayStoreApp/Main.java
+++ task01/src/com/example/PlayStoreApp/Main.java
@@ -3,6 +3,8 @@ package com.example.PlayStoreApp;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -16,12 +18,24 @@ public class Main {
 
   public static void main(String[] args) {
     
-    try (FileReader fr = new FileReader(args[0])) {
+    // To change to args[0] later.
+    try (FileReader fr = new FileReader("googleplaystore.csv")) {
       BufferedReader br = new BufferedReader(fr);
       
+      // This array or list is to get all the categories.
 
+
+      // Need something to keep track of the COUNT and DISCARDED.
 
       
+
+      // This map is for the final output.
+      Map<String, List<PlaystoreData>> categorised = br.lines()
+        .skip()
+
+
+
+
     } catch (IOException e) {
       e.printStackTrace();
     }
--- task01/src/com/example/PlayStoreApp/PlaystoreData.java
+++ task01/src/com/example/PlayStoreApp/PlaystoreData.java
@@ -2,7 +2,37 @@ package com.example.PlayStoreApp;
 
 public class PlaystoreData {
 
+  private String appName;
+  private Float rating;
+  private String category;
 
+  public PlaystoreData(String appName, Float rating, String category) {
+    this.appName = appName;
+    this.rating = rating;
+    this.category = category;
+  }
+  
+
+
+
+
+
+
+
+
+
+  // Getters and Setters
+  public String getAppName() { return appName; }
+
+  public void setAppName(String appName) { this.appName = appName; }
+
+  public Float getRating() { return rating; }
+
+  public void setRating(Float rating) { this.rating = rating; }
+
+  public String getCategory() { return category; }
+
+  public void setCategory(String category) { this.category = category; }
 
 
 }
